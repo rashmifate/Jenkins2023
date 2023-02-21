e7cff6b5f006c15f1f3c04e8ce8b10fedac00156
rashmi <rashmifate9595@gmail.com>
2023-02-21 13:11:28 +0530

    first changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	
 
 	public static void main(String[] args) {
-		System.out.println("Hello world through cmd maven");
+		System.out.println("Hello world through cmd maven first changes 21feb");
 		// TODO Auto-generated method stub
 
 	}
