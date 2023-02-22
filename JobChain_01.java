271a20eba3549a095bf67a60c337fd24ab1b4feb
rashmi <rashmifate9595@gmail.com>
2023-02-22 11:23:06 +0530

    add job chaining code

---
+++ JobChain_01.java
@@ -0,0 +1,11 @@
+
+public class JobChain_01 {
+	
+
+	public static void main(String[] args) {
+		System.out.println("This is parent job chaining code");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JobChain_02.java
@@ -0,0 +1,11 @@
+
+public class JobChain_02 {
+	
+
+	public static void main(String[] args) {
+		System.out.println("This is child job chaining");
+		// TODO Auto-generated method stub
+
+	}
+
+}
